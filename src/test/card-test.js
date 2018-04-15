137f500f7bddd31ac841d9c53b6034f5687ef1ca
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-04-15 17:11:23 -0600

    Switch from military time to standard time

---
+++ src/test/card-test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../card';
+import { shallow , mount } from 'enzyme';
+
+describe('Card tests', () => {
+
+  it('should return ten-day card if day property is present', () => {
+    
+  });
+
+  it('should return seven-hour card if day property is not present', () => {
+
+  });
+});
